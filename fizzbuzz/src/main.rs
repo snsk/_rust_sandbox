78efde2813919b91fa70127a0bfa192c5e9dd5ee
snsk <snsk@users.noreply.github.com>
2023-06-01 05:46:01 +0000

    add fizzbuzz

---
+++ fizzbuzz/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    for i in 0..100{
+        if i % 3 == 0 && i % 5 == 0{
+            println!("fizzbuzz")
+        }else if i % 3 == 0{
+            println!("fizz")
+        }else if i % 5 == 0{
+            println!("buzz")
+        }
+        else{
+            println!("{i}")
+        }
+    }
+}
